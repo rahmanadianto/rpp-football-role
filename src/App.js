6f5bbb0acf653979377f211449169f3f0daa3dd8
Rahman Adianto <rahman.adianto@gmail.com>
2016-12-13 00:54:04 +0700

    Add Roboto font

--- src/App.js
+++ src/App.js
@@ -74,188 +74,203 @@ class App extends Component {
       <div className='App-body'>
 
         <SelectField
-          floatingLabelText="Dribbling: Kemampuan pemain untuk menguasai bola"
+          floatingLabelText='Dribbling: Kemampuan pemain untuk menguasai bola'
           value={this.state.dribbling}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeDribbling}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Finishing: Kemampuan pemain untuk memaksimalkan peluang untuk mencetak goal"
+          floatingLabelText='Finishing: Kemampuan pemain untuk memaksimalkan peluang untuk mencetak goal'
           value={this.state.finishing}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
-          onChange={this.handleChangeCrossing}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+          onChange={this.handleChangeFinishing}>
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Crossing: Kemampuan pemain untuk memberikan umpan lambung dari sisi kotak penalti"
+          floatingLabelText='Crossing: Kemampuan pemain untuk memberikan umpan lambung dari sisi kotak penalti'
           value={this.state.crossing}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeCrossing}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Heading: Kemampuan dan keakuratan sundulan pemain"
+          floatingLabelText='Heading: Kemampuan dan keakuratan sundulan pemain'
           value={this.state.heading}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeHeading}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Marking: Kemampuan pemain untuk mengawal pemain lawan dan mencegah mendapat bola"
+          floatingLabelText='Marking: Kemampuan pemain untuk mengawal pemain lawan dan mencegah mendapat bola'
           value={this.state.marking}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeMarking}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Passing: Kemampuan pemain untuk memberikan umpan kepada rekan"
+          floatingLabelText='Passing: Kemampuan pemain untuk memberikan umpan kepada rekan'
           value={this.state.passing}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangePassing}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Tackling: Kemampuan pemain untuk melakukan tackling yang aman atau bersih"
+          floatingLabelText='Tackling: Kemampuan pemain untuk melakukan tackling yang aman atau bersih'
           value={this.state.tackling}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeTackling}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Agility: Kelincahan pemain secara umum"
+          floatingLabelText='Agility: Kelincahan pemain secara umum'
           value={this.state.agility}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeAgility}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Balance: Keseimbangan tubuh pemain"
+          floatingLabelText='Balance: Keseimbangan tubuh pemain'
           value={this.state.balance}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeBalance}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Speed: Kemampuan pemain untuk berlari baik saat membawa bola atau tidak"
+          floatingLabelText='Speed: Kemampuan pemain untuk berlari baik saat membawa bola atau tidak'
           value={this.state.speed}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeSpeed}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Stamina: Daya tahan pemain selama pertandingan"
+          floatingLabelText='Stamina: Daya tahan pemain selama pertandingan'
           value={this.state.stamina}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeStamina}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Strength: Kekuatan pemain"
+          floatingLabelText='Strength: Kekuatan pemain'
           value={this.state.strength}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeStrength}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Vision: Kemampuan pemain untuk mengamati daerah sekitarnya"
+          floatingLabelText='Vision: Kemampuan pemain untuk mengamati daerah sekitarnya'
           value={this.state.vision}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeVision}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Anticipation: Kemampuan pemain untuk menangani hal-hal krusial"
+          floatingLabelText='Anticipation: Kemampuan pemain untuk menangani hal-hal krusial'
           value={this.state.anticipation}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeAnticipation}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
 
         <SelectField
-          floatingLabelText="Goalkeeping: Kemampuan pemain untuk mempertahankan gawang dari kebobolan"
+          floatingLabelText='Goalkeeping: Kemampuan pemain untuk mempertahankan gawang dari kebobolan'
           value={this.state.goalkeeping}
           style={{fontSize: 24, padding: 6}}
+          floatingLabelStyle={{color: '#616161'}}
           fullWidth={true}
           onChange={this.handleChangeGoalkeeping}>
-            <MenuItem value={0} primaryText="Bad" />
-            <MenuItem value={1} primaryText="Medium" />
-            <MenuItem value={2} primaryText="Good" />
+            <MenuItem value={0} primaryText='Bad' />
+            <MenuItem value={1} primaryText='Medium' />
+            <MenuItem value={2} primaryText='Good' />
         </SelectField>
         <br />
         <br />
 
         <RaisedButton 
-          label="Submit" fullWidth={true} 
+          label='Submit' fullWidth={true} 
           primary={true} 
           style={{margin: 12, height: 50}}
           labelStyle={{fontSize: 24}}
@@ -267,7 +282,7 @@ class App extends Component {
 
   renderLoading() {
     return(
-      <div className="Loading">
+      <div className='Loading'>
         <CircularProgress size={100} thickness={7} />
       </div>
     )
